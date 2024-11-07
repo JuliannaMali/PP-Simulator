488303bdc9b5c7a2aa59d9fd62c8caf2e33af243
JuliannaMali <mali.julianna6@gmail.com>
2024-11-07 20:27:40 +0100

    Prostokąty

--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -6,68 +6,47 @@ internal class Program
     {
         Console.WriteLine("Starting Simulator!\n");
 
-        /*        Lab4a();
-                Lab4b();*/
-
-        Point p = new(10, 25);
-        Console.WriteLine(p.Next(Direction.Up));
-        Console.WriteLine(p.Next(Direction.Right));
-        Console.WriteLine(p.Next(Direction.Down));
-        Console.WriteLine(p.Next(Direction.Left));
-        Console.WriteLine(p.Next(Direction.Up).Next(Direction.Up));
-
+        Lab5a();
     }
 
-    static void Lab4a()
+    static void Lab5a()
     {
-        Console.WriteLine("HUNT TEST\n");
-        var o = new Orc() { Name = "Gorbag", Rage = 7 };
-        o.SayHi();
-        for (int i = 0; i < 10; i++)
+        Console.Write("Dwa poprawne prostokąty, r1[(1,2), (3,4)], r2[(0,0), (6,6)] \nOczekiwany wynik: r1 (1,2) : (3,4); r2 (0,0), (6,6) \nWynik: \n");
+        Rectangle r1 = new(1, 2, 3, 4);
+        Console.WriteLine(r1);
+        Rectangle r2 = new(0, 0, 6, 6);
+        Console.WriteLine(r2);
+
+        Console.Write("\nKonstruktor z pointem, r3[(1,1), (2,2) \nOczekiwany wynik: (1,1) : (2,2)] \nWynik: \n");
+        Point p01 = new(1, 1);
+        Point p02 = new(2, 2);
+        Rectangle r3 = new(p01, p02);
+        Console.WriteLine(r3);
+
+        Console.Write("\nZamienione współrzędne, r4[(4,7), (0,3)] \nOczekiwany wynik: (0,3) : (4,7) \nWynik: \n");
+        Rectangle r4 = new(4, 7, 0, 3);
+        Console.WriteLine(r4);
+
+        Console.Write("\nWspółliniowe współrzędne, r5[(1,3), (1,6)]\nOczekiwany wynik: Błąd\n");
+        try
         {
-            o.Hunt();
-            o.SayHi();
+            Rectangle r5 = new(1, 3, 1, 6);
+            Console.WriteLine(r5);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
         }
 
-        Console.WriteLine("\nSING TEST\n");
-        var e = new Elf("Legolas", agility: 2);
-        e.SayHi();
-        for (int i = 0; i < 10; i++)
+        Console.Write("\nZawieranie w r2, p1(1,2), p2(3,5), p3(7,2), p4(1,8), p5(4,4)\nOczekiwany wynik: True, True, False, False, True");
+        Point[] punkty =
+            [
+            new (1,2), new(3,5), new(7,2), new(1,8), new(4,4)
+            ];
+        foreach (Point i in punkty)
         {
-            e.Sing();
-            e.SayHi();
+            Console.WriteLine($"\n{r2.Contains(i)}");
         }
 
-        Console.WriteLine("\nPOWER TEST\n");
-        Creature[] creatures = {
-        o,
-        e,
-        new Orc("Morgash", 3, 8),
-        new Elf("Elandor", 5, 3)
-    };
-        foreach (Creature creature in creatures)
-        {
-            Console.WriteLine($"{creature.Name,-15}: {creature.Power}");
-        }
     }
-
-    static void Lab4b()
-    {
-        object[] myObjects = {
-        new Animals() { Description = "dogs"},
-        new Birds { Description = "  eagles ", Size = 10 },
-        new Elf("e", 15, -3),
-        new Orc("morgash", 6, 4)
-    };
-        Console.WriteLine("\nMy objects:");
-        foreach (var o in myObjects) Console.WriteLine(o);
-        /*
-            My objects:
-            ANIMALS: Dogs <3>
-            BIRDS: Eagles (fly+) <10>
-            ELF: E## [10][0]
-            ORC: Morgash [6][4]
-        */
-    }
-
 }
---
+++ Simulator/Rectangle.cs
@@ -0,0 +1,56 @@
+﻿namespace Simulator;
+
+internal class Rectangle
+{
+    public readonly int X1, Y1;
+    public readonly int X2, Y2;
+
+    public Rectangle(int x1, int y1, int x2, int y2)
+    {
+
+        if (x1 == x2 || y1==y2)
+        {
+           throw new ArgumentException("Podano złe współrzędne, nie utworzą one prostokąta");
+        }
+
+
+        if (x1>x2)
+        {
+            X1 = x2;
+            X2 = x1;
+
+            Y1 = y2;
+            Y2 = y1;
+ 
+        }
+        else
+        {
+            X1 = x1;
+            X2 = x2;
+
+            Y1 = y1;
+            Y2 = y2;
+        }
+       
+    }
+
+    public Rectangle(Point p1, Point p2) : this(p1.X, p1.Y, p2.X, p2.Y)
+    {
+
+    }
+
+    public bool Contains(Point point)
+    {
+        if(X1<point.X && point.X<X2 && Y1<point.Y && point.Y<Y2)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    public override string ToString()
+    {
+        return $"({X1},{Y1}) : ({X2},{Y2})";
+    }
+}
