ebe6876cf0479ad234da0a6753ee3aafda01f575
JuliannaMali <mali.julianna6@gmail.com>
2024-12-07 19:45:05 +0100

    Duża mapa

---
+++ Simulator/Maps/BigBounceMap.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator.Maps;
+
+public class BigBounceMap : BigMap
+{
+    public readonly int Size;
+    public BigBounceMap(int sizeX, int sizeY) : base(sizeX, sizeY) { } 
+
+    public override Point Next(Point p, Direction d)
+    {
+        if(p.X == Size -1 && d == Direction.Right)
+        {
+            return p.Next(Direction.Left);
+        }
+        else if(p.X == 0 && d == Direction.Left)
+        {
+            return p.Next(Direction.Right);
+        }
+        else if(p.Y == Size - 1 && d == Direction.Up)
+        {
+            return p.Next(Direction.Down);
+        }
+        else if(p.Y == 0 && d == Direction.Down)
+        {
+            return p.Next(Direction.Up);
+        }
+        return p.Next(d);
+    }
+
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        if (p.X == 0 && p.Y == 0)
+        {
+            switch (d)
+            {
+                case Direction.Right:
+                    return p;
+                case Direction.Left:
+                    return p;
+                case Direction.Down:
+                    return p.NextDiagonal(Direction.Up);
+            }
+        }
+        else if (p.X == SizeX - 1 && p.Y == 0)
+        {
+            switch (d)
+            {
+                case Direction.Right:
+                    return p.NextDiagonal(Direction.Left);
+                case Direction.Down:
+                    return p;
+                case Direction.Up:
+                    return p;
+            }
+        }
+        else if (p.X == SizeX - 1 && p.Y == SizeY - 1)
+        {
+            switch (d)
+            {
+                case Direction.Right:
+                    return p;
+                case Direction.Left:
+                    return p;
+                case Direction.Up:
+                    return p.NextDiagonal(Direction.Down);
+            }
+        }
+        else if (p.X == 0 && p.Y == Size - 1)
+        {
+            switch (d)
+            {
+                case Direction.Left:
+                    return p.NextDiagonal(Direction.Right);
+                case Direction.Down:
+                    return p;
+                case Direction.Up:
+                    return p;
+            }
+        }
+        else if (p.Y == 0)
+        {
+            switch (d)
+            {
+                case Direction.Right:
+                    return p.NextDiagonal(Direction.Left);
+                case Direction.Down:
+                    return p.NextDiagonal(Direction.Up);
+            }
+        }
+        else if (p.X == SizeX - 1)
+        {
+            switch (d)
+            {
+                case Direction.Up:
+                    return p.NextDiagonal(Direction.Down);
+                case Direction.Right:
+                    return p.NextDiagonal(Direction.Left);
+            }
+        }
+        else if (p.Y == SizeY - 1)
+        {
+            switch (d)
+            {
+                case Direction.Up:
+                    return p.NextDiagonal(Direction.Down);
+                case Direction.Left:
+                    return p.NextDiagonal(Direction.Right);
+            }
+        }
+        else if (p.X == 0)
+        {
+            switch (d)
+            {
+                case Direction.Left:
+                    return p.NextDiagonal(Direction.Right);
+                case Direction.Down:
+                    return p.NextDiagonal(Direction.Up);
+            }
+        }
+
+        return p.NextDiagonal(d);
+
+    }
+}
---
+++ Simulator/Maps/BigMap.cs
@@ -0,0 +1,64 @@
+﻿namespace Simulator.Maps;
+
+public abstract class BigMap : Map
+{
+
+    Dictionary<Point, List<IMappable>> _fields;
+    protected BigMap(int sizeX, int sizeY) : base(sizeX, sizeY)
+    {
+        if (sizeX>1000)
+        {
+            throw new ArgumentException(nameof(sizeX), "Too wide");
+        }
+        if (sizeY > 1000)
+        {
+            throw new ArgumentException(nameof(sizeY), "Too high");
+        }
+
+        _fields = new Dictionary<Point, List<IMappable>> ();
+
+    }
+
+    public override void Add(IMappable mappable, Point p)
+    {
+        if (!_fields.ContainsKey(p))
+        {
+            _fields[p] = new List<IMappable>();
+            
+        }
+        
+        _fields[p].Add(mappable);
+
+        mappable.InitMapAndPosition(this, p);
+    }
+
+    public override void Remove(IMappable mappable, Point p)
+    {
+        _fields[p].Remove(mappable);
+    }
+
+    public override List<IMappable>? At(int x, int y)
+    {
+        var p = new Point(x, y);
+        if (_fields.ContainsKey(p))
+        {
+            return _fields[p];
+        }
+        return new List<IMappable>();
+    }
+
+    public override List<IMappable>? At(Point p)
+    {
+        if (_fields.ContainsKey(p))
+        {
+            return _fields[p];
+        }
+        return new List<IMappable>();
+    }
+
+    public override void Move(IMappable mappable, Point p, Point p2)
+    {
+        Remove(mappable, p);
+        Add(mappable, p2);
+    }
+}
