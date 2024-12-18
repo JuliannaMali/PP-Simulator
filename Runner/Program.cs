744fac8773b939dda17a3f0e7fa56a53f4f6d7ca
JuliannaMali <mali.julianna6@gmail.com>
2024-12-18 16:03:33 +0100

    Sort

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Simulator.Maps;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Simulator;
 
@@ -6,7 +7,18 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        List<Creature> creatures = [new Orc("Gorbag", 6, 3)];
+        List<Creature> creatures = [new Orc("Gorbag", 6, 3), new Elf("Elandor", 4, 6), new Orc("Orc2", 3, 4)];
+
+        Func<Creature, Creature, int> PowerComp = (Creature creature1, Creature creature2) => creature1.Power.CompareTo(creature2.Power);
+
+        
+
+        creatures.Sort(PowerComp.Invoke);
+
+        foreach(Creature creature in creatures)
+        {
+            Console.WriteLine($"{creature} - {creature.Power}");
+        }
     }
  
 }
