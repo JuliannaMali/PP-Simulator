60d0b5a6c28979756c0321d39f09ea3fe6662e33
JuliannaMali <mali.julianna6@gmail.com>
2024-12-18 16:03:21 +0100

    Delegaty

--- Simulator/Maps/MoveRules.cs
+++ Simulator/Maps/MoveRules.cs
@@ -1,4 +1,6 @@
-﻿namespace Simulator.Maps;
+﻿using System.Drawing;
+
+namespace Simulator.Maps;
 
 internal static class MoveRules
 {
@@ -14,5 +16,202 @@ internal static class MoveRules
         var moved = p.NextDiagonal(d);
         if (!m.Exist(moved)) return p;
         return moved;
+    }
+
+    public static Point TorusNext(Map m, Point p, Direction d)
+    {
+        if (p.X == m.SizeX - 1 && d == Direction.Right)
+        {
+            return new Point(0, p.Y);
+        }
+        else if (p.X == 0 && d == Direction.Left)
+        {
+            return new Point(m.SizeX - 1, p.Y);
+        }
+        else if (p.Y == m.SizeY - 1 && d == Direction.Up)
+        {
+            return new Point(p.X, 0);
+        }
+        else if (p.Y == 0 && d == Direction.Down)
+        {
+            return new Point(p.X, m.SizeY - 1);
+        }
+        return p.Next(d);
+    }
+
+    public static Point TorusNextDiagonal(Map m, Point p, Direction d)
+    {
+        if (p.X == 0 && p.Y == 0 && d == Direction.Down)
+        {
+            return new Point(m.SizeX - 1, m.SizeY - 1);
+        }
+        else if (p.X == m.SizeX - 1 && p.Y == 0 && d == Direction.Right)
+        {
+            return new Point(0, m.SizeY - 1);
+        }
+        else if (p.X == m.SizeX - 1 && p.Y == m.SizeY - 1 && d == Direction.Up)
+        {
+            return new Point(0, 0);
+        }
+        else if (p.X == 0 && p.Y == m.SizeY - 1 && d == Direction.Left)
+        {
+            return new Point(m.SizeX - 1, 0);
+        }
+        else if (p.X == 0)
+        {
+            switch (d)
+            {
+                case Direction.Down:
+                    return new Point(m.SizeX - 1, p.Y - 1);
+                case Direction.Left:
+                    return new Point(m.SizeX - 1, p.Y + 1);
+            }
+        }
+        else if (p.X == m.SizeX - 1)
+        {
+            switch (d)
+            {
+                case Direction.Up:
+                    return new Point(0, p.Y + 1);
+                case Direction.Right:
+                    return new Point(0, p.Y - 1);
+            }
+        }
+        else if (p.Y == 0)
+        {
+            switch (d)
+            {
+                case Direction.Right:
+                    return new Point(p.X + 1, m.SizeY - 1);
+                case Direction.Down:
+                    return new Point(p.X - 1, m.SizeY - 1);
+            }
+        }
+        else if (p.Y == m.SizeY - 1)
+        {
+            switch (d)
+            {
+                case Direction.Left:
+                    return new Point(p.X - 1, 0);
+                case Direction.Up:
+                    return new Point(p.X + 1, 0);
+            }
+        }
+        return p.NextDiagonal(d);
+    }
+
+    public static Point BounceNext(Map m, Point p, Direction d)
+    {
+        if (p.X == m.SizeX - 1 && d == Direction.Right)
+        {
+            return p.Next(Direction.Left);
+        }
+        else if (p.X == 0 && d == Direction.Left)
+        {
+            return p.Next(Direction.Right);
+        }
+        else if (p.Y == m.SizeY - 1 && d == Direction.Up)
+        {
+            return p.Next(Direction.Down);
+        }
+        else if (p.Y == 0 && d == Direction.Down)
+        {
+            return p.Next(Direction.Up);
+        }
+        return p.Next(d);
+    }
+
+    public static Point BounceNextDiagonal(Map m, Point p, Direction d)
+    {
+        if (p.X == 0 && p.Y == 0)
+        {
+            switch (d)
+            {
+                case Direction.Right:
+                    return p;
+                case Direction.Left:
+                    return p;
+                case Direction.Down:
+                    return p.NextDiagonal(Direction.Up);
+            }
+        }
+        else if (p.X == m.SizeX - 1 && p.Y == 0)
+        {
+            switch (d)
+            {
+                case Direction.Right:
+                    return p.NextDiagonal(Direction.Left);
+                case Direction.Down:
+                    return p;
+                case Direction.Up:
+                    return p;
+            }
+        }
+        else if (p.X == m.SizeX - 1 && p.Y == m.SizeY - 1)
+        {
+            switch (d)
+            {
+                case Direction.Right:
+                    return p;
+                case Direction.Left:
+                    return p;
+                case Direction.Up:
+                    return p.NextDiagonal(Direction.Down);
+            }
+        }
+        else if (p.X == 0 && p.Y == m.SizeY - 1)
+        {
+            switch (d)
+            {
+                case Direction.Left:
+                    return p.NextDiagonal(Direction.Right);
+                case Direction.Down:
+                    return p;
+                case Direction.Up:
+                    return p;
+            }
+        }
+        else if (p.Y == 0)
+        {
+            switch (d)
+            {
+                case Direction.Right:
+                    return p.NextDiagonal(Direction.Left);
+                case Direction.Down:
+                    return p.NextDiagonal(Direction.Up);
+            }
+        }
+        else if (p.X == m.SizeX - 1)
+        {
+            switch (d)
+            {
+                case Direction.Up:
+                    return p.NextDiagonal(Direction.Down);
+                case Direction.Right:
+                    return p.NextDiagonal(Direction.Left);
+            }
+        }
+        else if (p.Y == m.SizeY - 1)
+        {
+            switch (d)
+            {
+                case Direction.Up:
+                    return p.NextDiagonal(Direction.Down);
+                case Direction.Left:
+                    return p.NextDiagonal(Direction.Right);
+            }
+        }
+        else if (p.X == 0)
+        {
+            switch (d)
+            {
+                case Direction.Left:
+                    return p.NextDiagonal(Direction.Right);
+                case Direction.Down:
+                    return p.NextDiagonal(Direction.Up);
+            }
+        }
+
+        return p.NextDiagonal(d);
     }
 }
--- Simulator/Maps/SmallTorusMap.cs
+++ Simulator/Maps/SmallTorusMap.cs
@@ -4,88 +4,10 @@ namespace Simulator.Maps;
 
 public class SmallTorusMap : SmallMap
 {
-
     public readonly int Size;
-    public SmallTorusMap(int sizeX, int sizeY) : base(sizeX, sizeY) { }
-    public override Point Next(Point p, Direction d)
+    public SmallTorusMap(int sizeX, int sizeY) : base(sizeX, sizeY) 
     {
-        if(p.X==SizeX-1 && d == Direction.Right)
-        {
-            return new Point (0, p.Y);
-        }
-        else if(p.X == 0 && d == Direction.Left)
-        {
-            return new Point(SizeX - 1, p.Y);
-        }
-        else if(p.Y == SizeY -1 && d == Direction.Up)
-        {
-            return new Point(p.X, 0);
-        }
-        else if(p.Y == 0 && d == Direction.Down)
-        {
-            return new Point(p.X, SizeY - 1);
-        }
-        return p.Next(d);
-    }
-
-    public override Point NextDiagonal(Point p, Direction d)
-    {
-        if (p.X == 0 && p.Y == 0 && d == Direction.Down)
-        {
-            return new Point(SizeX - 1, SizeY - 1);
-        }
-        else if (p.X == SizeX - 1 && p.Y == 0 && d == Direction.Right)
-        {
-            return new Point(0, SizeY - 1);
-        }
-        else if (p.X == SizeX -1 && p.Y == SizeY - 1 && d == Direction.Up)
-        {
-            return new Point(0, 0);
-        }
-        else if (p.X == 0 && p.Y == SizeY -1 && d == Direction.Left)
-        {
-            return new Point(SizeX - 1, 0);
-        }
-        else if (p.X == 0)
-        {
-            switch(d)
-            {
-                case Direction.Down:
-                    return new Point(SizeX - 1, p.Y - 1);
-                case Direction.Left:
-                    return new Point(SizeX - 1, p.Y + 1);
-            }   
-        }
-        else if (p.X == SizeX - 1)
-        {
-            switch(d)
-            {
-                case Direction.Up:
-                    return new Point(0, p.Y + 1);
-                case Direction.Right:
-                    return new Point(0, p.Y - 1);
-            }
-        }
-        else if (p.Y == 0)
-        {
-            switch(d)
-            {
-                case Direction.Right:
-                    return new Point(p.X + 1, SizeY - 1);
-                case Direction.Down:
-                    return new Point(p.X - 1, SizeY - 1);
-            }
-        }
-        else if (p.Y == SizeY - 1)
-        {
-            switch(d)
-            {
-                case Direction.Left:
-                    return new Point(p.X - 1, 0);
-                case Direction.Up:
-                    return new Point(p.X + 1, 0);
-            }
-        }
-        return p.NextDiagonal(d);
+        FNext = MoveRules.TorusNext;
+        FNextDiagonal = MoveRules.TorusNextDiagonal;
     }
 }
